441d059f4582af585f70863a1ea035d911a3bb70
thealinivan <ivana@roehampton.ac.uk>
2021-07-27 16:31:23 +0100

    AI: Fixed UI and functional issues

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const round = no => { return Math.round(no * 100) / 100 }
 
 //scrape
 const scrape = (betHouse, url) => {
+    //to be replaced with scraping algorithm
     data = url;
     return data
 }
@@ -94,7 +95,7 @@ const calculateWinningOdds = (maxOddsMatches) => {
             wOdds[2].bet = round(favBet);
             wOdds[2].return = round(favBetr);
 
-            //static
+            //to be integrated as part of the scrapping component
             wOdds[2].status = "won";
 
             wOdds.sort((function (a, b) { return a.id - b.id; }));
@@ -110,44 +111,50 @@ const calculateWinningOdds = (maxOddsMatches) => {
     return winningMatches;
 }
 
+//FE
 //reder data to DOM
 const renderToDOM = winOdds => {
-
 
     winOdds.forEach(match => {
         let currentOdds = [match.left, match.draw, match.right];
         currentOdds.sort((function (a, b) { return a.return - b.return; }));
-        totalBetAcumulator += match.left.bet + match.draw.bet + match.right.bet;
-        minReturnAcumulator += currentOdds[0].return;
-        maxReturnAcumulator += currentOdds[2].return;
+
         const game = `
-            <div id=${match.id} class="card padd-1 marg-1">
-                <h4 class="padd-1 center-text">${match.leftTeam} vs ${match.rightTeam}</h4>
-                <p>Total bet: <b>${match.left.bet + match.draw.bet + match.right.bet}</b></p>
-                <p>Min return: <b>${currentOdds[0].return}</b></p>
-                <p>Max return: <b>${currentOdds[2].return}</b></p>
-                
+            <div id=${match.id} class="match-item bd padd-1 center-text d-inline-block">
+                <h4 class="padd-1 center-text deep-gray">${match.leftTeam} vs ${match.rightTeam}</h4>
+                <div>
+                <p class="bd gray d-inline-block">Total bet: <b>${match.left.bet + match.draw.bet + match.right.bet}</b></p>
+                <p class="bd gray d-inline-block">Min return: <b>${currentOdds[0].return}</b></p>
+                <p class="bd gray d-inline-block">Max return: <b>${currentOdds[2].return}</b></p>
+                </div>
+                <br>
                 <div>
                     <div id=${match.left.id} class="${match.left.status} bd padd-1 d-inline-block">
-                        <p class="light-back"><b>${match.left.type}</b></p>
-                        <p>${match.left.odd}</p>
-                        <p>${match.left.betHouse}</p>
-                        <p>Bet: <b>${match.left.bet}</b></p>
-                        <p>Return: <b>${match.left.return}</b></p>
+                        <p class="light-back deep-gray"><b>${match.left.type}</b></p>
+                        <br>
+                        <h5><b>${match.left.odd}</b></h5>
+                        <p class="deep-gray">${match.left.betHouse}</p>
+                        <br>
+                        <p class="gray">Bet: <b>${match.left.bet}</b></p>
+                        <p class="gray">Return: <b>${match.left.return}</b></p>
                     </div>
                     <div id=${match.draw.id} class="${match.draw.status} bd padd-1 d-inline-block">
-                        <p class="light-back"><b>${match.draw.type}</b></p>
-                        <p>${match.draw.odd}</p>
-                        <p>${match.draw.betHouse}</p>
-                        <p>Bet: <b>${match.draw.bet}</b></p>
-                        <p class=>Return: <b>${match.draw.return}</b></p>
+                        <p class="light-back deep-gray"><b>${match.draw.type}</b></p>
+                        <br>
+                        <h5><b>${match.draw.odd}</b></h5>
+                        <p class="deep-gray">${match.draw.betHouse}</p>
+                        <br>
+                        <p class="gray">Bet: <b>${match.draw.bet}</b></p>
+                        <p class="gray">Return: <b>${match.draw.return}</b></p>
                     </div>
                     <div id=${match.right.id} class="${match.right.status} bd padd-1 d-inline-block">
-                        <p class="light-back"><b>${match.right.type}</b></p>
-                        <p>${match.right.odd}</p>
-                        <p>${match.right.betHouse}</p>
-                        <p>Bet: <b>${match.right.bet}</b></p>
-                        <p>Return: <b>${match.right.return}</b></p>
+                        <p class="light-back deep-gray"><b>${match.right.type}</b></p>
+                        <br>
+                        <h5><b>${match.right.odd}</b></h5>
+                        <p class="deep-gray">${match.right.betHouse}</p>
+                        <br>
+                        <p class="gray">Bet: <b>${match.right.bet}</b></p>
+                        <p class="gray">Return: <b>${match.right.return}</b></p>
                     </div>
                 </div>
             </div>
@@ -155,12 +162,45 @@ const renderToDOM = winOdds => {
         const list = document.getElementById(`games-list`);
         $(list).append(game);
     })
-    document.getElementById("total-games").innerHTML = `Current Games: <b>${maxOdds.length}</b>`;
-    document.getElementById("bet-games").innerHTML = `Betted Games: <b>${winningOdds.length}</b>`;
-    document.getElementById("total-bet").innerHTML = `Total Bet: <b>${round(totalBetAcumulator)}</b>`;
-    document.getElementById("min-return").innerHTML = `Min Return: <b>${round(minReturnAcumulator)}</b>`;
-    document.getElementById("max-return").innerHTML = `Max Return: <b>${round(maxReturnAcumulator)}</b>`;
-    document.getElementById("max-profit").innerHTML = `Max Profit: <b>${round(maxReturnAcumulator - totalBetAcumulator)}</b>`;
+    document.getElementById("total-games").innerHTML = `<b>${maxOdds.length}</b>`;
+    document.getElementById("bet-games").innerHTML = `<b>${winningOdds.length}</b>`;
+    document.getElementById("total-bet").innerHTML = `<b>${round(totalBetAcumulator)}</b>`;
+    document.getElementById("min-return").innerHTML = `<b>${round(minReturnAcumulator)}</b>`;
+    document.getElementById("max-return").innerHTML = `<b>${round(maxReturnAcumulator)}</b>`;
+    document.getElementById("max-profit").innerHTML = `<b>${round(maxReturnAcumulator - totalBetAcumulator)}</b>`;
+}
+
+//FE
+//search
+const search = () => {
+    const output = document.getElementById("games-list");
+    output.innerHTML = "";
+    let filtered = maxOdds.filter(match => (match.leftTeam.toLowerCase() + match.rightTeam.toLowerCase())
+        .includes($('#field-search').val().toLowerCase()));
+    $('#field-search').val() == "" ? renderToDOM(winningOdds) : renderToDOM(filtered);
+}
+
+//bet
+const bettWinningOdds = winningOdds => {
+
+    winningOdds.forEach(match => {
+        //get stats
+        let wOdds = [match.left, match.draw, match.right];
+        wOdds.sort((function (a, b) { return a.odd - b.odd; })).reverse();
+        totalBetAcumulator += match.left.bet + match.draw.bet + match.right.bet;
+        minReturnAcumulator += wOdds[0].return;
+        maxReturnAcumulator += wOdds[2].return;
+
+        //initiate betting
+        wOdds.forEach(bet => {
+            console.log(`House ${bet.betHouse} | Type: ${bet.type} | Odds: ${bet.odd} | Bet: ${bet.bet}`);
+        })
+    });
+
+
+    //update UI
+    //(to be replaced with sending response to client)
+    renderToDOM(winningOdds);
 }
 
 
@@ -274,9 +314,14 @@ const housesData = [betHouse0, betHouse1, betHouse2];
 const maxOdds = calculateMaxOdds(housesData);
 const winningOdds = calculateWinningOdds(maxOdds);
 console.log(winningOdds);
+bettWinningOdds(winningOdds);
 
-
-renderToDOM(winningOdds);
+//event listeners
+document.getElementById('field-search').addEventListener('keyup', (event) => {
+    event.preventDefault();
+    console.log("keyup");
+    search();
+})
 
 
 
