c9d41c37889774a144a1375c15dfeb56c49906e1
thealinivan <ivana@roehampton.ac.uk>
2021-07-27 14:14:22 +0100

    AI: Fixed bug to sort array of winning odds

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const initBet = 10.00;
+let totalBetAcumulator = 0.0;
+let minReturnAcumulator = 0.0;
+let maxReturnAcumulator = 0.0;
+
+
 //FUNCTIONS
 
 //round
@@ -19,18 +25,25 @@ const calculateMaxOdds = hData => {
             leftTeam: m1.leftTeam,
             rightTeam: m1.rightTeam,
             left: {
+                id: 1,
+                type: "1",
                 betHouse: m1.betHouse,
                 odd: m1.left
             },
             draw: {
+                id: 2,
+                type: "X",
                 betHouse: m1.betHouse,
                 odd: m1.draw
             },
             right: {
+                id: 3,
+                type: "2",
                 betHouse: m1.betHouse,
                 odd: m1.right
             },
         };
+
         hData.forEach(house2 => {
             house2.forEach(m2 => {
                 if (m1.id === m2.id) {
@@ -57,54 +70,34 @@ const calculateMaxOdds = hData => {
 //filter winning odds
 const calculateWinningOdds = (maxOddsMatches) => {
     let winningMatches = [];
-    maxOddsMatches.forEach(m1 => {
 
-        let match = {
-            id: m1.id,
-            betHouse: m1.betHouse,
-            leftTeam: m1.leftTeam,
-            rightTeam: m1.rightTeam,
-            left: {
-                betHouse: m1.betHouse,
-                odd: m1.left,
-                bet: 0.0,
-                return: 0.0
-            },
-            draw: {
-                betHouse: m1.betHouse,
-                odd: m1.draw,
-                bet: 0.0,
-                return: 0.0
-            },
-            right: {
-                betHouse: m1.betHouse,
-                odd: m1.right,
-                bet: 0.0,
-                return: 0.0
-            },
-        };
+    maxOddsMatches.forEach(match => {
+        let wOdds = [match.left, match.draw, match.right];
+        wOdds.sort((function (a, b) { return a.odd - b.odd; })).reverse();
 
-        let wOdds = [m1.left, m1.draw, m1.right];
-        // wOdds.sort((function (a, b) { return a.odd - b.odd; }));
-
-        let bet0 = 10.00
-        let bet1 = round(bet0 * wOdds[0].odd / wOdds[1].odd)
-        let favBet = round(bet0 * wOdds[0].odd - (bet0 + bet1))
-        let bet0r = round(bet0 * wOdds[0].odd)
-        let bet1r = round(bet1 * wOdds[1].odd)
-        let favBetr = round(favBet * wOdds[2].odd)
-        let totalBet = round(bet0 + bet1 + favBet)
-        let minR = round(bet0 * wOdds[0].odd)
-        let maxR = round(favBet * wOdds[2].odd)
-        let profit = round(maxR - totalBet)
+        let bet0 = initBet
+        let bet1 = bet0 * wOdds[0].odd / wOdds[1].odd
+        let favBet = bet0 * wOdds[0].odd - (bet0 + bet1)
+        let bet0r = bet0 * wOdds[0].odd
+        let bet1r = bet1 * wOdds[1].odd
+        let favBetr = favBet * wOdds[2].odd
+        let totalBet = bet0 + bet1 + favBet
+        let minR = bet0 * wOdds[0].odd
+        let maxR = favBet * wOdds[2].odd
+        let profit = maxR - totalBet
 
         if (profit > 0) {
-            wOdds[0].bet = 100.0;
-            wOdds[0].return = bet0r;
-            wOdds[1].bet = bet1;
-            wOdds[1].return = bet1r;
-            wOdds[2].bet = favBet;
-            wOdds[2].return = favBetr;
+            wOdds[0].bet = round(initBet);
+            wOdds[0].return = round(bet0r);
+            wOdds[1].bet = round(bet1);
+            wOdds[1].return = round(bet1r);
+            wOdds[2].bet = round(favBet);
+            wOdds[2].return = round(favBetr);
+
+            //static
+            wOdds[2].status = "won";
+
+            wOdds.sort((function (a, b) { return a.id - b.id; }));
 
             match.left = wOdds[0];
             match.draw = wOdds[1];
@@ -115,55 +108,63 @@ const calculateWinningOdds = (maxOddsMatches) => {
 
     })
     return winningMatches;
-
 }
 
 //reder data to DOM
-const renderToDOM = maxOdds => {
-    document.getElementById("bets-no").innerHTML = `Current Games: ${maxOdds.length}`;
-    maxOdds.forEach(match => {
+const renderToDOM = winOdds => {
 
+
+    winOdds.forEach(match => {
+        let currentOdds = [match.left, match.draw, match.right];
+        currentOdds.sort((function (a, b) { return a.return - b.return; }));
+        totalBetAcumulator += match.left.bet + match.draw.bet + match.right.bet;
+        minReturnAcumulator += currentOdds[0].return;
+        maxReturnAcumulator += currentOdds[2].return;
         const game = `
-     <div id=${match.id} class="card padd-1 marg-1">
-                    <h4 class="padd-1 center-text">${match.leftTeam} vs ${match.rightTeam}</h4>
-                    <div>
-                        <div class="card padd-1 d-inline-block">
-                            <p class="light-back"><b>1</b></p>
-                            <p>${match.left.odd}</p>
-                            <p>${match.left.betHouse}</p>
-                            <p>Amount: <b>${match.left.bet}</b></p>
-                            <p>Return: <b>${match.left.return}</b></p>
-                            <p>Status: <b>./</b></p>
-                        </div>
-                        <div class="card padd-1 d-inline-block">
-                            <p class="light-back"><b>X</b></p>
-                            <p>${match.draw.odd}</p>
-                            <p>${match.draw.betHouse}</p>
-                             <p>Amount: <b>${match.draw.bet}</b></p>
-                            <p>Return: <b>${match.draw.return}</b></p>
-                            <p>Status: <b>./</b></p>
-                        </div>
-                        <div class="card padd-1 d-inline-block">
-                            <p class="light-back"><b>2</b></p>
-                            <p>${match.right.odd}</p>
-                            <p>${match.right.betHouse}</p>
-                            <p>Amount: <b>${match.right.bet}</b></p>
-                            <p>Return: <b>${match.right.return}</b></p>
-                            <p>Status: <b>./</b></p>
-                        </div>
+            <div id=${match.id} class="card padd-1 marg-1">
+                <h4 class="padd-1 center-text">${match.leftTeam} vs ${match.rightTeam}</h4>
+                <p>Total bet: <b>${match.left.bet + match.draw.bet + match.right.bet}</b></p>
+                <p>Min return: <b>${currentOdds[0].return}</b></p>
+                <p>Max return: <b>${currentOdds[2].return}</b></p>
+                
+                <div>
+                    <div id=${match.left.id} class="${match.left.status} bd padd-1 d-inline-block">
+                        <p class="light-back"><b>${match.left.type}</b></p>
+                        <p>${match.left.odd}</p>
+                        <p>${match.left.betHouse}</p>
+                        <p>Bet: <b>${match.left.bet}</b></p>
+                        <p>Return: <b>${match.left.return}</b></p>
+                    </div>
+                    <div id=${match.draw.id} class="${match.draw.status} bd padd-1 d-inline-block">
+                        <p class="light-back"><b>${match.draw.type}</b></p>
+                        <p>${match.draw.odd}</p>
+                        <p>${match.draw.betHouse}</p>
+                        <p>Bet: <b>${match.draw.bet}</b></p>
+                        <p class=>Return: <b>${match.draw.return}</b></p>
+                    </div>
+                    <div id=${match.right.id} class="${match.right.status} bd padd-1 d-inline-block">
+                        <p class="light-back"><b>${match.right.type}</b></p>
+                        <p>${match.right.odd}</p>
+                        <p>${match.right.betHouse}</p>
+                        <p>Bet: <b>${match.right.bet}</b></p>
+                        <p>Return: <b>${match.right.return}</b></p>
                     </div>
                 </div>
-    `;
+            </div>
+        `;
         const list = document.getElementById(`games-list`);
         $(list).append(game);
     })
+    document.getElementById("total-games").innerHTML = `Current Games: <b>${maxOdds.length}</b>`;
+    document.getElementById("bet-games").innerHTML = `Betted Games: <b>${winningOdds.length}</b>`;
+    document.getElementById("total-bet").innerHTML = `Total Bet: <b>${round(totalBetAcumulator)}</b>`;
+    document.getElementById("min-return").innerHTML = `Min Return: <b>${round(minReturnAcumulator)}</b>`;
+    document.getElementById("max-return").innerHTML = `Max Return: <b>${round(maxReturnAcumulator)}</b>`;
+    document.getElementById("max-profit").innerHTML = `Max Profit: <b>${round(maxReturnAcumulator - totalBetAcumulator)}</b>`;
 }
 
 
-
-//LOGIC
-
-//INIT
+//DATA
 
 //target urls
 url0 = [
@@ -172,9 +173,9 @@ url0 = [
         betHouse: "paddypower",
         leftTeam: "Spain",
         rightTeam: "Italy",
-        left: 1.5,
+        left: 3.5,
         draw: 1.9,
-        right: 4.7
+        right: 2.6
     },
     {
         id: "match2",
@@ -182,6 +183,15 @@ url0 = [
         leftTeam: "England",
         rightTeam: "Germany",
         left: 3.8,
+        draw: 2.0,
+        right: 1.2
+    },
+    {
+        id: "match3",
+        betHouse: "paddypower",
+        leftTeam: "Denmark",
+        rightTeam: "Romania",
+        left: 2.7,
         draw: 2.0,
         right: 1.2
     }
@@ -193,9 +203,9 @@ url1 = [
         betHouse: "ladbrokes",
         leftTeam: "Spain",
         rightTeam: "Italy",
-        left: 2.5,
-        draw: 3.1,
-        right: 3.1
+        left: 2.3,
+        draw: 2.7,
+        right: 2.3
     },
     {
         id: "match2",
@@ -205,6 +215,15 @@ url1 = [
         left: 2.5,
         draw: 2.65,
         right: 2.3
+    },
+    {
+        id: "match3",
+        betHouse: "ladbrokes",
+        leftTeam: "Denmark",
+        rightTeam: "Romania",
+        left: 2.6,
+        draw: 3.3,
+        right: 3.4
     }
 ]
 
@@ -214,7 +233,7 @@ url2 = [
         betHouse: "bet365",
         leftTeam: "England",
         rightTeam: "Germany",
-        left: 2.5,
+        left: 2.7,
         draw: 2.3,
         right: 2.7
     },
@@ -223,11 +242,22 @@ url2 = [
         betHouse: "bet365",
         leftTeam: "Spain",
         rightTeam: "Italy",
-        left: 2.35,
-        draw: 5.2,
-        right: 2.7
+        left: 2.3,
+        draw: 2.2,
+        right: 3.1
+    },
+    {
+        id: "match3",
+        betHouse: "bet365",
+        leftTeam: "Denmark",
+        rightTeam: "Romania",
+        left: 2.5,
+        draw: 2.9,
+        right: 3.95
     }
 ]
+
+//LOGIC
 
 //house names
 bh0 = "paddypower"
@@ -244,6 +274,7 @@ const housesData = [betHouse0, betHouse1, betHouse2];
 const maxOdds = calculateMaxOdds(housesData);
 const winningOdds = calculateWinningOdds(maxOdds);
 console.log(winningOdds);
+
 
 renderToDOM(winningOdds);
 
